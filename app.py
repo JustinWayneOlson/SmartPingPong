f1d89bf57c5d95c3c33ccca008f52eff4d74b54d
justin.olson <justin.olson@workday.com>
2017-11-30 16:34:57 -0700

    added sample endpoint, static recources, point prod server to build dir

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
 import tornado.ioloop
+import tornado.autoreload
+import os
 import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('./public/index.html')
+        self.render('./build/index.html')
+
+class TestEndpointHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write({'response': 'hello'})
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r'/testEndpoint', TestEndpointHandler),
+        (r'/static/js/(.*)',tornado.web.StaticFileHandler, {"path": "./build/static/js"},),
+        (r'/static/css/(.*)',tornado.web.StaticFileHandler, {"path": "./build/static/css"},),
+        (r'/static/media/(.*)',tornado.web.StaticFileHandler, {"path": "./build/static/media"},)
     ])
 
 if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
     print("serving on port 8888")
+
+    #TODO remove in prod
+    tornado.autoreload.start()
+    for dir, _, files in os.walk('./'):
+        [tornado.autoreload.watch(dir + '/' + f) for f in files if not f.startswith('.')]
+
     tornado.ioloop.IOLoop.current().start()
