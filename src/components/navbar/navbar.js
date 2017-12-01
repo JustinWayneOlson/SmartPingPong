83507cb6a0ff98d641c36ecdd5a018507898e106
justin.olson <justin.olson@workday.com>
2017-11-30 19:54:37 -0700

    wip

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+
 import './navbar.css';
 
 class Navbar extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div className="Navbar">
+        <div className="organization-name">
+          {"Boulder Pong"}
+        </div>
+        <div className="avatar">
+
+        </div>
       </div>
     );
   }
