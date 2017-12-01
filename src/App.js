a1e0e2f5fb2fff5f703cddbf1f6925c8b5b73b21
joziannesb <joziannesb@gmail.com>
2017-11-30 20:09:39 -0700

    2 navbar component (#14)
    Added nav bar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar/navbar.js'
 
+import './App.css'
+
 class App extends Component {
   render() {
     return(
-      <div>
+      <div className="App">
         <Navbar/>
         <h1> Hello World </h1>
       </div>
