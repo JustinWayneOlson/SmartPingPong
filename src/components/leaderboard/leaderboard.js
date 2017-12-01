d68c00586c91cb8dab76b0f0b8e415637a1a64c6
Jozi Brennan <joziannesb@gmail.com>
2017-12-01 04:05:22 -0700

    made leaderboard with singles and doubles tabs

--- src/components/leaderboard/leaderboard.js
+++ src/components/leaderboard/leaderboard.js
@@ -3,10 +3,67 @@ import React, { Component } from 'react';
 import './leaderboard.css';
 
 class Leaderboard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {selectedTab:"Singles"}
+  }
+
+  selectTab(tab) {
+    if(tab==="Singles") {
+      this.setState({selectedTab:"Singles"})
+    }
+    if(tab==="Doubles") {
+      this.setState({selectedTab:"Doubles"})
+    }
+  }
+
   render() {
+    const isActiveTab = (selectedTab) => {
+      if (selectedTab === this.state.selectedTab) {
+        return 'active'
+      } else {
+        return ''
+      }
+    }
     return (
       <div className="Leaderboard">
-        {"Shit I will deal with later"}
+        <div className="singles-or-doubles">
+          <div className={`singles tab ${isActiveTab("Singles")}`} onClick={()=>{this.selectTab("Singles")}}>
+            {"Singles"}
+          </div>
+          <div className={`doubles tab ${isActiveTab("Doubles")}`}  onClick={()=>{this.selectTab("Doubles")}}>
+            {"Doubles"}
+          </div>
+        </div>
+        {
+          this.state.selectedTab === "Singles" ? (
+            <div className="singles leaderboard-tab">
+              <div>
+                {"Name"}
+              </div>
+              <div className="avatar">
+              </div>
+              <div>
+                {"W/L"}
+              </div>
+            </div>
+          ) : (
+            <div className="doubles leaderboard-tab">
+              <div>
+                {"Name & Name"}
+              </div>
+              <div className="team-avatars">
+                <div className="avatar">
+                </div>
+                <div className="avatar">
+                </div>
+              </div>
+              <div>
+                {"W/L"}
+              </div>
+            </div>
+          )
+        }
       </div>
     );
   }
