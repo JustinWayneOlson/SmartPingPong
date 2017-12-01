e19c571fdf2af592a9d6e750c8e71e7bf77cad44
joziannesb <joziannesb@gmail.com>
2017-11-30 23:27:20 -0700

    made modal for queue (#20)
    * made modal for queue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import Navbar from './components/navbar/navbar.js'
 import Scoreboard from './components/scoreboard/scoreboard.js'
+import Modal from './components/modal/modal.js'
 
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {modalOpen:false}
+  }
   render() {
     return(
       <div className="App">
         <Navbar/>
         <Scoreboard/>
+        <button className="Queue-button" onClick={() => {
+          this.setState({modalOpen:true})
+        }}> Queue </button>
+        {
+          this.state.modalOpen === true ? <Modal onClose={() => {
+            this.setState({ modalOpen: false })
+          }}/> : null
+        }
         <h1> Hello World </h1>
       </div>
     )
